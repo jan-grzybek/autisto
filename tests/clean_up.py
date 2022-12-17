be2b12fa3cf84f0b56e2cce6ef521f2c27eb194b
Jan Grzybek <lyre_embassy_0n@icloud.com>
2022-12-17 17:41:34 +0100

    wip

--- tests/clean_up.py
+++ tests/clean_up.py
@@ -1,9 +1,10 @@
-import os
+import sys
+import json
 import gspread
 
 
 def clean_up():
-    gc = gspread.service_account_from_dict(os.environ["CREDENTIALS"])
+    gc = gspread.service_account_from_dict(json.loads(sys.argv[1]))
     for title in [sh.title for sh in gc.openall()]:
         print(title)
         gc.del_spreadsheet(title)
