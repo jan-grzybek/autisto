574fd3ed46479beec3ed31db62f0a1ff331f1eb0
Jan Grzybek <lyre_embassy_0n@icloud.com>
2022-12-17 17:29:44 +0100

    wip

--- tests/spreadsheet.py
+++ tests/spreadsheet.py
@@ -2,8 +2,16 @@ import pytest
 import gspread
 from autisto.spreadsheet import get_config
 
-def test_spreadsheet_generation():
+
+@pytest.fixture
+def spreadsheet():
     config = get_config()
     gc = gspread.service_account_from_dict(config["credentials"])
     name = f"Inventory {config['spreadsheet_uuid']}"
-    gc.open(name)
+    return gc.open(name)
+
+
+def test_worksheets_generation(spreadsheet):
+    for name in ["Console", "Inventory", "Spending"]:
+        _ = spreadsheet.worksheet(name)
+    assert True
