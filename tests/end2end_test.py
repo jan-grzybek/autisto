8e13a1712bf7f498444500c5cc15b848fa8101a2
Jan Grzybek <lyre_embassy_0n@icloud.com>
2022-12-17 16:55:16 +0100

    wip

--- tests/end2end_test.py
+++ tests/end2end_test.py
@@ -1,4 +1,9 @@
 import pytest
+import gspread
+from autisto.spreadsheet import get_config
 
 def test_spreadsheet_generation():
-
+    config = get_config()
+    gc = gspread.service_account_from_dict(config["credentials"])
+    name = f"Inventory {config['spreadsheet_uuid']}"
+    gc.open(name)
