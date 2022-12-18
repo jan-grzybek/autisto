fc13cb79611e229ec312d6293672dfb3d0b020d5
Jan Grzybek <lyre_embassy_0n@icloud.com>
2022-12-18 19:12:11 +0100

    wip

--- tests/test_autisto.py
+++ tests/test_autisto.py
@@ -138,18 +138,18 @@ def test_adding(spreadsheet):
                 row_values.append(item_data[col_name])
             except KeyError:
                 row_values.append("")
-    console.update(f"{ALPHABET[START_COL]}5:{ALPHABET[START_COL+len(CONSOLE_COL_NAMES)-1]}5", [row_values])
+    console.update(f"{ALPHABET[START_COL]}5:{ALPHABET[START_COL + len(CONSOLE_COL_NAMES) - 1]}5", [row_values])
 
     time.sleep(20)
     inventory = spreadsheet.worksheet("Inventory")
     total_value = item_data["Quantity"] * float(item_data["Unit price [PLN]"].replace(",", "."))
-    assert total_value == inventory.cell(to_1_based(START_ROW), START_COL+len(INVENTORY_COL_NAMES))
-    row_values = inventory.row_values[to_1_based(START_ROW)+2]
+    assert total_value == inventory.cell(to_1_based(START_ROW), START_COL + len(INVENTORY_COL_NAMES)).value
+    row_values = inventory.row_values[to_1_based(START_ROW) + 2]
     for i, col_name in enumerate(INVENTORY_COL_NAMES):
         if col_name in ["Category", "Item name", "Quantity", "Life expectancy [months]"]:
-            assert item_data[col_name] == row_values[i+START_ROW]
+            assert item_data[col_name] == row_values[i + START_ROW]
         elif col_name == "Average unit value [PLN]":
-            assert float(item_data["Unit price [PLN]"].replace(",", ".")) == float(row_values[i+START_ROW])
+            assert float(item_data["Unit price [PLN]"].replace(",", ".")) == float(row_values[i + START_ROW])
         elif col_name == "Total value [PLN]":
             assert total_value == float(row_values[i + START_ROW])
         elif col_name == "Depreciation [PLN]":
