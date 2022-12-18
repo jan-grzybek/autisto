89fda6df8340779177b9d711f19a679d59ccd089
Jan Grzybek <lyre_embassy_0n@icloud.com>
2022-12-18 11:56:05 +0100

    wip

--- tests/test_spreadsheet.py
+++ tests/test_spreadsheet.py
@@ -22,12 +22,14 @@ def spreadsheet():
     return gc.open(name)
 
 
+@pytest.mark.order2
 def test_sheets_creation(spreadsheet):
     for sheet in SHEET_NAMES:
         _ = spreadsheet.worksheet(sheet)
     assert True
 
 
+@pytest.mark.order3
 def test_column_titling(spreadsheet):
     sheets_to_titles = {"Console": CONSOLE_COL_NAMES, "Inventory": INVENTORY_COL_NAMES, "Spending": SPENDING_COL_NAMES}
     with lock:
@@ -38,36 +40,59 @@ def test_column_titling(spreadsheet):
                 assert row_values[i] == col_name
 
 
+@pytest.mark.order1
 def test_server_alive():
     platform = get_platform()
     assert platform.service_active()
 
 
+@pytest.mark.order4
 def test_sheets_auto_clean_up(spreadsheet):
-    def get_random_cell_coordinates():
-        return random.randint(1, 1000), random.randint(1, len(ALPHABET))
-    cells_to_litter_per_sheet = 3
+    class RandomCoordinates:
+        def __init__(self):
+            self.row = random.randint(1, 1000)
+            self.col = random.randint(1, len(ALPHABET))
+
+    litter = ["She's got the wings and teeth of a African bat"
+              "Her middle name is Mudbone and on top of all that"
+              "Your mama got a glass eye with the fish in it"]
     cells_to_litter = {sheet: [] for sheet in SHEET_NAMES}
     for sheet in cells_to_litter.keys():
-        for _ in range(cells_to_litter_per_sheet):
-            cells_to_litter[sheet].append(get_random_cell_coordinates())
+        for _ in range(len(litter)):
+            cells_to_litter[sheet].append(RandomCoordinates())
     for sheet in SHEET_NAMES:
         worksheet = spreadsheet.worksheet(sheet)
-        for i in range(cells_to_litter_per_sheet):
+        for i in range(len(litter)):
             worksheet.update_cell(
-                *cells_to_litter[sheet][i],
-                "She's got the wings and teeth of a African bat; "
-                "Her middle name is Mudbone and on top of all that; "
-                "Your mama got a glass eye with the fish in it"
+                cells_to_litter[sheet][i].row,
+                cells_to_litter[sheet][i].col,
+                litter[i]
             )
     time.sleep(10)
-    test_server_alive()
+
     for sheet in SHEET_NAMES:
         worksheet = spreadsheet.worksheet(sheet)
-        for i in range(cells_to_litter_per_sheet):
+        for i in range(len(litter)):
             cell_coordinates = cells_to_litter[sheet][i]
-            if cell_coordinates[0] in [to_1_based(START_ROW), to_1_based(START_ROW)+1] or \
-                    cell_coordinates[1] == to_1_based(START_COL):
-                continue
-            assert worksheet.cell(*cell_coordinates).value == ""
+            if sheet == "Console":
+                if cell_coordinates.row == 1:
+                    assert worksheet.cell(cell_coordinates.row, cell_coordinates.col).value == ""
+                elif cell_coordinates.col == 1 or to_1_based(START_COL) + len(CONSOLE_COL_NAMES) <= cell_coordinates.col:
+                    assert worksheet.cell(cell_coordinates.row, cell_coordinates.col).value == ""
+                elif cell_coordinates.row != 2:
+                    assert worksheet.cell(cell_coordinates.row, cell_coordinates.col).value == litter[i]
+            elif sheet == "Inventory":
+                if cell_coordinates.row == 1 or to_1_based(START_ROW) + 2 <= cell_coordinates.row:
+                    assert worksheet.cell(cell_coordinates.row, cell_coordinates.col).value == ""
+                elif cell_coordinates.col == 1 or \
+                        to_1_based(START_COL) + len(INVENTORY_COL_NAMES) <= cell_coordinates.col:
+                    assert worksheet.cell(cell_coordinates.row, cell_coordinates.col).value == ""
+            elif sheet == "Spending":
+                if cell_coordinates.row == 1 or to_1_based(START_ROW) + 1 <= cell_coordinates.row:
+                    assert worksheet.cell(cell_coordinates.row, cell_coordinates.col).value == ""
+                elif cell_coordinates.col == 1 or \
+                        to_1_based(START_COL) + len(SPENDING_COL_NAMES) <= cell_coordinates.col:
+                    assert worksheet.cell(cell_coordinates.row, cell_coordinates.col).value == ""
+            else:
+                assert False
     test_column_titling(spreadsheet)
